2c1e3a5289f8dd1c6cb2b8d6749bf482ee838572
sekwong <seksan404@gmail.com>
2016-06-07 15:47:39 +0700

    add swagger to config

--- app.js
+++ app.js
@@ -8,11 +8,31 @@ var server = new Hapi.Server();
 // Define PORT number
 server.connection({ port: 7002 });
 
+// Register Swagger Plugin ( Use for documentation and testing purpose )
+server.register({
+    register: require('hapi-swagger'),
+    options: {
+        apiVersion: "0.0.1"
+    }
+}, function (err) {
+    if (err) {
+        server.log(['error'], 'hapi-swagger load error: ' + err)
+    } else {
+        server.log(['start'], 'hapi-swagger interface loaded')
+    }
+});
+
 // =============== Routes for our API =======================
 // Define GET route
 server.route({
     method: 'GET',      // Methods Type
     path: '/api/user',  // Url
+    config: {
+        // Include this API in swagger documentation
+        tags: ['api'],
+        description: 'Get All User data',
+        notes: 'Get All User data'
+    },
     handler: function (request, reply) { //Action
 
         // Response JSON object
